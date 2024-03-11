8a3dcf3797c4999fb016403ec5ab8d6e71006285
“Nidhi <ps4151722@gmail.com>
2024-03-11 16:53:06 +0530

    done

--- discoveriesTask-Priyanshu/Controller/CustomCellTableViewCell.swift
+++ discoveriesTask-Priyanshu/Controller/CustomCellTableViewCell.swift
@@ -1,49 +1,36 @@
-//
-//  CustomCellTableViewCell.swift
-//  discoveriesTask-Priyanshu
-//
-//  Created by Zignuts Technolab on 08/03/24.
-//
-
 import UIKit
 
 class CustomCellTableViewCell: UITableViewCell {
+    
+  
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var accessoryButton: UIButton!
-    
     @IBOutlet weak var locationLabel: UILabel!
-    
     @IBOutlet weak var routeButtonStack: UIStackView!
     @IBOutlet weak var mainStackView: UIStackView!
     
+  
     var accessoryButtonTapHandler: (() -> Void)?
-        
-        @IBAction func accessoryButtonTapped(_ sender: Any) {
-            accessoryButtonTapHandler?()
-        }
+    
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+ 
+    @IBAction func accessoryButtonTapped(_ sender: Any) {
+        accessoryButtonTapHandler?()
+    }
+    
+  
     override func layoutSubviews() {
-            super.layoutSubviews()
+        super.layoutSubviews()
+        backgroundColor = UIColor.clear
         mainStackView.layer.cornerRadius = 10
         mainStackView.clipsToBounds = true
         routeButtonStack.layer.cornerRadius = 20
         routeButtonStack.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         routeButtonStack.isLayoutMarginsRelativeArrangement = true
-        
-        
-        }
-
-    
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-        backgroundColor = UIColor.clear
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 }
---
+++ discoveriesTask-Priyanshu/Controller/ViewController.swift
@@ -0,0 +1,114 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+    @IBOutlet weak var tablView: UITableView!
+    @IBOutlet weak var musicNumberStackView: UIStackView!
+    @IBOutlet weak var navigationView: UIView!
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var miniPlayerImage: UIImageView!
+    @IBOutlet weak var miniPlayerTitle: UILabel!
+    @IBOutlet weak var miniPlayerType: UILabel!
+    @IBOutlet weak var miniPlayer: UIView!
+    
+    
+    @IBAction func miniPlayerCancelButtonTapped(_ sender: UIButton) {
+        miniPlayer.isHidden = true
+    }
+    var items: [Item] = []
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        miniPlayer.isHidden = true
+        navigationController?.navigationBar.backgroundColor = .blue
+        
+        let backgroundImage = UIImage(named: "BackgroundImage")
+        view.backgroundColor = UIColor(patternImage: backgroundImage!)
+        mainView.backgroundColor = .clear
+        navigationView.layer.cornerRadius = 20
+        navigationView.clipsToBounds = true
+        
+        musicNumberStackView.layer.cornerRadius = 23
+        musicNumberStackView.clipsToBounds = true
+        musicNumberStackView.layoutMargins = UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16)
+        musicNumberStackView.isLayoutMarginsRelativeArrangement = true
+        
+        miniPlayer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            miniPlayer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            miniPlayer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            miniPlayer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            miniPlayer.heightAnchor.constraint(equalToConstant: 60)
+        ])
+        
+        tablView.register(UINib(nibName: "CustomCellTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
+        tablView.dataSource = self
+        tablView.delegate = self
+        
+        loadItems()
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items[section].items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCellTableViewCell
+        let item = items[indexPath.section].items[indexPath.row]
+        cell.titleLabel.text = item.title
+        cell.typeLabel.text = item.type
+        cell.locationLabel.text = item.location
+        cell.accessoryButton.setImage(UIImage(named: item.imageName), for: .normal)
+        
+        cell.accessoryButtonTapHandler = {
+            self.miniPlayerTitle.text = item.title
+            self.miniPlayerType.text = item.type
+            self.miniPlayerImage.image = UIImage(named: item.imageName)
+            self.miniPlayer.isHidden = false
+        }
+        
+        return cell
+    }
+}
+
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        let titleLabel = UILabel(frame: CGRect(x: 16, y: 10, width: tableView.frame.width, height: 20))
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        titleLabel.textColor = .buttonColorGrey
+        titleLabel.text = items[section].date
+        headerView.addSubview(titleLabel)
+        return headerView
+    }
+}
+
+
+private extension ViewController {
+    func loadItems() {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+            print("JSON file not found.")
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let decoder = JSONDecoder()
+            items = try decoder.decode([Item].self, from: data)
+            tablView.reloadData()
+        } catch {
+            print("Error reading or parsing JSON:", error)
+        }
+    }
+}
